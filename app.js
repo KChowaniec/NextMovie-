e45e392b7e2d7382e6ca45b8311031902237b507
warrior <warriorbrant@gmail.com>
2016-08-12 17:08:58 -0400

    new

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(cookieParser());
 app.engine('handlebars', handlebarsInstance.engine);
 app.set('view engine', 'handlebars');
 
-/*
+
 app.use(function (request, response, next) {
     if ((request.cookies.next_movie == undefined || (new Date(request.cookies.next_movie.expires) < new Date(Date.now()))) 
         && request.originalUrl != "/login" && request.originalUrl != "/user/login" && request.originalUrl != "/user/register"
@@ -64,7 +64,7 @@ app.use(function (request, response, next) {
     
     next();
 });
-*/
+
 
 configRoutes(app);
 
