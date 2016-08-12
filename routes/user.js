173293766c5aa9b9fb73a1262369c084f99aab12
warrior <warriorbrant@gmail.com>
2016-08-12 13:14:09 -0400

    new

--- routes/user.js
+++ routes/user.js
@@ -68,11 +68,11 @@ router.post('/user/register', function (req, res) {
 											user.password = user.hashedPassword;
 											user.username = user.profile.username;
 											users.verifyUser(user).then((userObj) => {
-											res.cookie("next_movie", userObj.sessionId, { expires: new Date(Date.now() + 24 * 3600000), httpOnly: true });
-											res.json({ success: true });
-											return;
+												res.cookie("next_movie", userObj.sessionId, { expires: new Date(Date.now() + 24 * 3600000), httpOnly: true });
+												res.json({ success: true });
+												return;
+											});
 										});
-									});
 								} else {
 									res.json({ success: false, message: "Registration is failed" });
 								}
@@ -82,7 +82,7 @@ router.post('/user/register', function (req, res) {
 						res.json({ success: false, message: "Registration is failed because the email has already existed" });
 						throw "email alreay exists!"
 					}
-			})
+			   })
 		} else{
 			res.json({ success: false, message: "Registration is failed because the username has already existed" });
 			throw "username already exists!"
